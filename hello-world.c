647ea449a9d4f536ad540436750ea6c18304c54e
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2024-09-02 11:41:12 +0800

    Add personalized greeting feature.

--- hello-world.c
+++ hello-world.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
 int main() {
-    printf("Hello, World! I am Mark Allen Jugalbot.\n");
+    char name[100];
+
+    printf("Hello, World! Please input your name to start:\n");
+    gets(name);
+    printf("Welcome back, %-5s", name);
 
     return 0;
 }
