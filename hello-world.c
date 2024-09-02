7f03b12538b56320eec461542980b5f1a480482d
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2024-09-02 11:46:08 +0800

    test conflict

--- hello-world.c
+++ hello-world.c
@@ -3,7 +3,7 @@
 int main() {
     char name[100];
 
-    printf("Hello, World! Please input your name to start:\n");
+    printf("Hello, World! Please input your name to startddd:\n");
     gets(name);
     printf("Welcome back, %-5s", name);
 
